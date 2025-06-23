c2e68a5fae0caa030445b8bc35120aaec5f99f89
Omar Assem <omarassem.route@gmail.com>
2025-06-23 21:54:55 +0300

    Change initial route to welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class XOGame extends StatelessWidget {
         WelcomeScreen.routeName: (_) => WelcomeScreen(),
         GameBoardScreen.routeName: (_) => GameBoardScreen(),
       },
-      initialRoute: GameBoardScreen.routeName,
+      initialRoute: WelcomeScreen.routeName,
     );
   }
 }
